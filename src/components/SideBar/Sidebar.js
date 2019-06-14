bc86163539e13f67e5dbf720532e0cd1d705804c
adebola <bola@ringley.co.uk>
2019-06-14 02:23:53 +0100

    restructure after adding  redux to the app

--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -7,18 +7,17 @@ import UpdateBasket from "../UpdateBasket";
 import TotalCost from "../TotalCost";
 import Header from "../Header"; 
 
-const SideBar = ({ toggleVisibility, basket  , updateProductUnit}) => {
+const SideBar = ({toggleVisibility}) => {
   return (
     <div className="modal">
       <div className="modal-content-container">
         <div className="modal-content">
           <Header
             toggleVisibility={toggleVisibility}
-            basket={basket}
           />
           <h2>My Basket</h2>
-          <UpdateBasket basket={basket} updateProductUnit={updateProductUnit}/>
-          <TotalCost basket={basket}/>
+          <UpdateBasket />
+          <TotalCost />
         </div>
       </div>
     </div>
@@ -27,8 +26,6 @@ const SideBar = ({ toggleVisibility, basket  , updateProductUnit}) => {
 
 SideBar.propTypes ={
   toggleVisibility: PropTypes.func.isRequired,
-  basket: PropTypes.arrayOf(PropTypes.object).isRequired,
-  updateProductUnit: PropTypes.func.isRequired,
 }
 
 export default SideBar;
