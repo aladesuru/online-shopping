48fa141355c329decacf684bb65b352b7b2eab0c
adebola <bola@ringley.co.uk>
2019-06-14 13:07:05 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-
 //imported components
 import Header from "./components/Header";
 import ProductList from "./components/ProductCardList";
@@ -9,16 +8,6 @@ import SideBar from "./components/SideBar";
 
 class App extends Component {
   state = {
-    basket: [
-      {
-        code: "p60216526",
-        name: "Cotton Rich Knitted Polo",
-        image:
-          "https://asset1.cxnmarksandspencer.com/is/image/mands/SD_03_T30_2412M_F0_X_EC_90",
-        price: "19.50",
-        units: 1
-      }
-    ],
     isVisible: false
   };
 
